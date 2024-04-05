d9e7e84b8f602511f2fb8c20ceedd440ea60d655
HSN-CODES <logics.nook@gmail.com>
2024-04-05 13:03:50 +0500

    "class 2"

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,7 +1,9 @@
 import { NavLink } from "react-router-dom";
 import { Link } from "react-router-dom";
-import { FaFacebook, FaInstagram, FaTwitter } from "react-icons/fa6";
+import { FaBars, FaFacebook, FaInstagram, FaTwitter } from "react-icons/fa6";
+import { useState } from "react";
 const Header = () => {
+  const [navOpen, setNavOpen] = useState(false);
   return (
     <>
       <header>
@@ -9,7 +11,6 @@ const Header = () => {
         <nav
           className="nav-strip h-[25px] p-[4px] bg-primary text-white
          grid grid-cols-[100px,100px,100px,1fr]
-         
         "
         >
           <section className=" flex  justify-between">
@@ -31,6 +32,29 @@ const Header = () => {
               Register
             </Link>
           </section>
+        </nav>
+        {/* main nav */}
+        <nav className="p-2 grid grid-cols-2">
+          <h1 className="text-2xl">Task-App</h1>
+          <button
+            className="justify-self-end text-[30px] md:hidden"
+            onClick={() => {
+              setNavOpen(!navOpen);
+            }}
+          >
+            <FaBars />
+          </button>
+          <ul
+            className={`px-2 flex flex-col md:flex-row ${
+              !navOpen && "h-0"
+            } md:justify-self-end  overflow-hidden gap-2 md:h-auto md:gap-4 text-[17px]`}
+          >
+            <li>Login</li>
+            <li>Rgister</li>
+            <li>Rgister</li>
+            <li>Rgister</li>
+            <li>Rgister</li>
+          </ul>
         </nav>
       </header>
     </>
